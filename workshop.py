d2df017f62ae604062a08ad818cf738888f20c8a
S Luck <ssc.luck@gmail.com>
2013-10-05 15:56:51 -0400

    Refactored code, added a test

--- workshop.py
+++ workshop.py
@@ -1,51 +1,74 @@
 from random import *
+import unittest
 
-isInteger = False
+class MyTest(unittest.TestCase):
+	def test_guess(self):
+		return None
 
-while (not isInteger):
-	lower_bound_str = raw_input('Enter lower bound:')
-	upper_bound_str = raw_input('Enter upper bound:')
-
-	try:
-		lower_bound = int(lower_bound_str)
-		upper_bound = int(upper_bound_str)
-		isInteger = True
-	except:
-		print 'Please enter an integer.'
-
-
-
-number = randint(lower_bound, upper_bound)
-guess = number+1
-
-
-tries = 0
-while (guess != number):
+def getInt(prompt):
+	# Given: a string as a prompt for the user
+	# Returns: the integer inputted by user
+	# 	Prompts the user to re-enter input if non-integer
 	isInteger = False
 
+	# Keep prompting user until input is int
 	while (not isInteger):
-		guess_str = raw_input('Enter your guess:')
+		input_str = raw_input(prompt)
 
 		try:
-			guess = int(guess_str)
+			input_num = int(input_str)
 			isInteger = True
 		except:
 			print 'Please enter an integer.'
 
-	if (guess < lower_bound) or (guess > upper_bound):
-		print 'Out of bounds :('
-		continue
+	return input_num
 
-	if (guess > number):
-		print 'Go lower'
+def setBounds():
+	# Prompts user for bounds
+	# Returns bounds as tuple: (lower_bound, upper_bound)
 
-	elif (guess < number):
-		print 'Go higher'
+	isValid = False
+
+	while (not isValid):
+		lower_bound = getInt('Enter a lower bound:')
+		upper_bound = getInt('Enter an upper bound:')
+
+		if (lower_bound <= upper_bound)
+			isValid = True
+
+		else
+			print 'Invalid bounds'
+
+	return (lower_bound, upper_bound)
+
+def playGame(lower_bound, upper_bound):
+	# Given: range for guesses
+	# Generate random number and prompts the user through guesses until guess is
+	# 	random number
+
+	number = randint(lower_bound, upper_bound)
+	guess = number+1
+
+	tries = 0
+	while (guess != number):
+		guess = getInt('Enter your guess:')
+		
+		if (guess < lower_bound) or (guess > upper_bound):
+			print 'Out of bounds :('
+			continue
+
+		if (guess > number):
+			print 'Go lower'
+
+		elif (guess < number):
+			print 'Go higher'
 
 
-	tries += 1
+		tries += 1
 
-print 'Yay! It took you ', tries, ' tries to guess the number.'
+	assert(guess == number)
+
+	print 'Yay! It took you ', tries, ' tries to guess the number.'
 
 
 
